81a4f440b4d27bab5884200f2eeba024d1b3efba
Sanchit Raj <sanchit.raj@belden.com>
2026-06-16 07:09:29 +0530

    first 16-06-2026

---
+++ array_hashing/TwoSum.java
@@ -0,0 +1,13 @@
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        
+        int nums [] = {2,7,11,15}, target = 9;
+
+        twoSum(nums, target);
+    }
+}
